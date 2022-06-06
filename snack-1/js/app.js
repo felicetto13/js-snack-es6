555de6a1d524496ae3fc0aeaab85b5b56ba9328b
felicetto13 <felice.laterza93@icloud.com>
2022-06-06 16:02:11 +0200

    first step first snack

---
+++ snack-1/js/app.js
@@ -0,0 +1,37 @@
+/* 
+
+Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa 
+con le seguenti proprietà: nome e peso.
+
+Stampare a schermo la bici con peso minore 
+utilizzando destructuring e template literal
+
+*/
+
+
+//ccreo array di oggetti 
+
+let bici = [
+    {
+    nome: "Atala meccanica",
+    peso: 37,
+},
+{
+    nome: "Scott Scott",
+    peso: 47,
+},
+{
+    nome: "Cannondale",
+    peso: 557,
+},
+{
+    nome: "Cervelo ",
+    peso: 24,
+},
+{
+    nome: "Orbea",
+    peso: 41,
+}
+]
